4d28434981ea5fff1eda728ae00e3adc7fd06a44
ProninaAnastasiia <soul_bler@mail.ru>
2023-10-31 00:05:51 +0300

    little fix

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Dependency_Inversion;
+﻿using Dependency_Inversion;
 
 Message a = new Message();
 a.Write();
 a.Send();
-Console.WriteLine("Hello, World!");
 MessageNotification mn = new MessageNotification(a);
 mn.SendNotification();
